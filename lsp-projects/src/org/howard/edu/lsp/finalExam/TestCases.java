0746d0aeced7e1882bb74f3c7368f754338b901e
zoeacarter <zoe.carter@bison.howard.edu>
2022-04-22 15:07:59 -0400

    Last time

--- lsp-projects/src/org/howard/edu/lsp/finalExam/TestCases.java
+++ lsp-projects/src/org/howard/edu/lsp/finalExam/TestCases.java
@@ -2,13 +2,27 @@ package org.howard.edu.lsp.finalExam;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.HashMap;
+
 import org.junit.jupiter.api.Test;
 
+import org.howard.edu.lsp.finalExam.*;
 class TestCases {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	
+	void commonKeyValuePairs() throws NullMapException{
+		HashMap<String, String> map1 = new HashMap<String, String>();
+		map1.put("1", "2");
+		map1.put("Z", "O");
+		HashMap<String, String> map2 = new HashMap<String, String>();
+		map2.put("Um", "Chile");
+		map2.put("1", "2");
+		
+		//assertEquals(2,commonKeyValuePairs(map1, map2));
+		
+	}
+		
 	}
 
-}
+
